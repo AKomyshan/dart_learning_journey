2e76258bdf91eecf6ff81ebf4b2b24515a26a9c7
antonkomyshan <anton.komyshan@namecheap.com>
2025-06-18 21:26:59 +0300

    chore: task 8

---
+++ lib/task8.dart
@@ -0,0 +1,21 @@
+/// Завдання 8: Цикли for
+/// 1) Виведіть всі числа від 1 до 10.
+/// 2) Виведіть тільки парні числа.
+/// 3) Обчисліть суму чисел від 1 до 10.
+(List<int> enumerate, List<int> evenOnlyEnumerate, int sum) task8(int count) {
+  final List<int> enumerate = [];
+  final List<int> evenOnlyEnumerate = [];
+  int sum = 0;
+
+  for(var i = 0; i < count; i++) {
+    final humanReadableIndex = i + 1;
+    enumerate.add(humanReadableIndex);
+    if (humanReadableIndex % 2 == 0) {
+      evenOnlyEnumerate.add(humanReadableIndex);
+    }
+
+    sum += humanReadableIndex;
+  }
+
+  return (enumerate, evenOnlyEnumerate, sum);
+}
---
+++ test/task8_test.dart
@@ -0,0 +1,20 @@
+import 'package:test/test.dart';
+import 'package:dart_learning_journey/task8.dart';
+
+void main() {
+  group('Task 8', () {
+    test('Should works', () {
+      // arrange
+      final int count = 10;
+
+      // act
+      final (List<int> enumerate, List<int> evenOnlyEnumerate, int sum) =
+          task8(count);
+
+      // assert
+      expect(enumerate, equals([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+      expect(evenOnlyEnumerate, equals([2, 4, 6, 8, 10]));
+      expect(sum, 55);
+    });
+  });
+}
