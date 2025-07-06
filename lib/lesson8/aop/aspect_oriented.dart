24f33174ff2ee665120c293783efc8953cf50f22
antonkomyshan <anton.komyshan@namecheap.com>
2025-07-06 19:02:08 +0300

    feat(DART-08): fix review comments

--- lib/lesson8/aop/aspect_oriented.dart
+++ lib/lesson8/aop/aspect_oriented.dart
@@ -25,25 +25,24 @@ class AspectOriented {
     _logger.info('Searching for @task functions using reflect...');
 
     final mirrorSystem = currentMirrorSystem();
-    final ClassMirror talkerClassMirror = reflectClass(Talker);
-    final List<_TaskEntry> tasksToRun = [];
+    final talkerClassMirror = reflectClass(Talker);
+    final tasksToRun = <_TaskEntry>[];
 
     // Since it's just homeworks skipped advanced checks what 'main' here since we have more than one main.
-    final Map<Uri, LibraryMirror> currentLibraries = mirrorSystem.libraries;
+    final currentLibraries = mirrorSystem.libraries;
 
-    for (final MapEntry<Uri, LibraryMirror> currentLibrary in currentLibraries.entries) {
+    for (final currentLibrary in currentLibraries.entries) {
       _logger.debug('Inspecting library: ${currentLibrary.key.toString()}');
 
-      for (final DeclarationMirror declaration in currentLibrary.value.declarations.values) {
-        if (declaration is MethodMirror && declaration.isStatic ||
-            (declaration is MethodMirror && declaration.isTopLevel)) {
-          for (final InstanceMirror metadata in declaration.metadata) {
+      for (final declaration in currentLibrary.value.declarations.values) {
+        if (declaration case MethodMirror(isStatic: true) || MethodMirror(isTopLevel: true)) {
+          for (final metadata in declaration.metadata) {
             if (metadata.reflectee is! Task) {
               continue;
             }
 
-            final String functionName = MirrorSystem.getName(declaration.simpleName);
-            final Task taskAnnotation = metadata.reflectee as Task;
+            final functionName = MirrorSystem.getName(declaration.simpleName);
+            final taskAnnotation = metadata.reflectee as Task;
 
             // Here is place for play with duck typing... if we want ofc:)
             if (declaration.parameters.length == 1 && declaration.parameters.first.type == talkerClassMirror) {
@@ -68,9 +67,9 @@ class AspectOriented {
 
     _logger.info('Running @task functions:');
     tasksToRun.sort((a, b) => a.order.compareTo(b.order));
-    for (int i = 0; i < tasksToRun.length; i++) {
-      final _TaskEntry taskEntry = tasksToRun[i];
-      final int taskNumber = i + 1;
+    for (var i = 0; i < tasksToRun.length; i++) {
+      final taskEntry = tasksToRun[i];
+      final taskNumber = i + 1;
       _logger.logCustom(TaskStartingLog(taskNumber));
 
       // Real act here!!!
